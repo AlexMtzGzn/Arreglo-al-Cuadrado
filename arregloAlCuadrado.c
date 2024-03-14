5771a9e880854f10966e942be8084ddf4b9cda64
AlexMtzGzn <alex13141926@gmail.com>
2024-03-13 22:34:32 -0600

    Declarcion de hilos y variable entera "elementos"
    Declarcion de hilos necesarios y solicitud al usuario de cantidad de elemntos del arreglo

--- arregloAlCuadrado.c
+++ arregloAlCuadrado.c
@@ -2,9 +2,15 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+
+
 int main(void){
 
+    pthread_t hilo1,hilo2;
+    int elementos; 
 
+    printf("\nIngresa la cantidad de elementos del arreglo: ");
+    scanf("%i",&elementos);
 
     return 0;
 }
